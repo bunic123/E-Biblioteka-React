d5ab533e71e5a772962cfeb681de0f0790cbaa91
bunic123 <bunic.fon@gmail.com>
2022-09-04 12:36:36 +0200

    Pretraga knjiga u biblioteci

--- src/komponente/NavMenu.js
+++ src/komponente/NavMenu.js
@@ -1,6 +1,14 @@
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 
 function NavMenu() {
+
+    const navigate = useNavigate();
+
+    function logout() {
+        localStorage.removeItem('admin');
+        navigate(0);
+    }
+
     return (
         <div className="nav-div">
             <nav>
@@ -8,6 +16,11 @@ function NavMenu() {
                 <Link to="/registracija"><li id="register-nm">Register</li></Link>
                 <Link to="/prijava"><li id="login-nm">Login</li></Link>
                 <Link to="/pretraga"><li id="search-nm">Search</li></Link>
+                {localStorage.getItem('admin') ?
+                    <button onClick={logout} className='btn btn-danger' id='lgt'>Log out</button>
+                    : ''}
+
+
             </nav>
         </div>
     )
--- src/komponente/SveKnjige.js
+++ src/komponente/SveKnjige.js
@@ -5,8 +5,11 @@ import Knjiga from './Knjiga';
 function SveKnjige() {
 
     const [sveKnjige, setSveKnjige] = useState([]);
+    const [nazivKnjige, setNazivKnjige] = useState('');
 
-
+    function handleNazivKnjige(e) {
+        setNazivKnjige(e.target.value)
+    }
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/vrati-sve-knjige/").then(res => {
@@ -16,17 +19,37 @@ function SveKnjige() {
     }, []);
 
 
+    function pretragaKnjiga() {
+        axios.get(`http://localhost:8000/api/pretraga/${nazivKnjige}`).then(res => {
+            setSveKnjige(res.data.knjige)
+        });
+
+    }
+
+
+
     const slika = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a7af54b9-3a91-433b-99e7-5274c8355a63/de5mmbu-2471ba83-d537-4754-bd90-acfd4d861b15.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E3YWY1NGI5LTNhOTEtNDMzYi05OWU3LTUyNzRjODM1NWE2M1wvZGU1bW1idS0yNDcxYmE4My1kNTM3LTQ3NTQtYmQ5MC1hY2ZkNGQ4NjFiMTUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.cKYJtbZdffgjGyjrAC1eUfqk4iRBMJDKBmLIgTeZ8Xw'
 
     return (
-        <div className='knjige-div'>
-            {sveKnjige.map(knjiga => {
-                return (
-                    <div className='prikaz-knjiga'>
-                        <Knjiga key={knjiga.id} knjiga={knjiga} slika={slika} />
-                    </div>
-                );
-            })}
+        <div>
+
+            <div>
+                <input type="text" id="src" className="form-control" value={nazivKnjige} onChange={handleNazivKnjige} placeholder="Unesite naziv knjige" />
+                <button onClick={pretragaKnjiga} className='btn btn-danger' id='btn-s'>Pretraga</button>
+            </div>
+
+            <div className='knjige-div'>
+
+
+                {sveKnjige.map(knjiga => {
+                    return (
+                        <div className='prikaz-knjiga'>
+                            <Knjiga key={knjiga.id} knjiga={knjiga} slika={slika} />
+                        </div>
+                    );
+                })}
+            </div>
+
         </div>
     )
 
